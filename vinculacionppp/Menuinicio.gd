05281f56fd8e1eb1f74db15b15250b6ebde3083b
CristinaVinanzaca <krisvinanzaca_99@hotmail.com>
2020-09-10 21:00:45 -0500

    fondos

--- vinculacionppp/Menuinicio.gd
+++ vinculacionppp/Menuinicio.gd
@@ -1,7 +1,8 @@
 extends Control
 
-
-
+func _ready():
+	Audio.get_node("musicaFondo").stream=load("res://sound/musica-instrumental-para-ninos-felices.ogg")
+	Audio.get_node("musicaFondo").play()
 func _on_Buttoninicionio_pressed():
 	get_tree().change_scene("res://escenas/niñoFrente.tscn")
 
