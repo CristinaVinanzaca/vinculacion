ced2b8b17f03edbe47f1b9bf8afd5da92fefecf5
CristinaVinanzaca <krisvinanzaca_99@hotmail.com>
2020-09-25 21:08:27 -0500

    nuevas escenas :100:

--- vinculacionppp/Menudehombre.gd
+++ vinculacionppp/Menudehombre.gd
@@ -1,7 +1,19 @@
 extends Control
-
-
-
+onready var btncabeza=get_node("cabeza")
+onready var btnhuesos=get_node("cabeza/OrganosHombres/Esqueletohombre")
+onready var btnorganos=get_node("cabeza/OrganosHombres")
+onready var botn=get_node("Button")
+func _ready():
+	btncabeza.connect("pressed",self,"_llamarCabeza")
+	btnhuesos.connect("pressed",self,"_llamarHuesos")
+	btnorganos.connect("pressed",self,"_llamarOrganos")
+	botn.connect("pressed",self,"_on_Button_pressed")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://escenas/niñoFrente.tscn")
+func _llamarCabeza():
+	get_tree().change_scene("res://escenas/niñoCabeza.tscn")
+func _llamarHuesos():
+	get_tree().change_scene("res://escenas/niñohuesos.tscn")
+func _llamarOrganos():
+	get_tree().change_scene("res://escenas/niñoOrganos.tscn")
--- vinculacionppp/escenas/Menudeopciones.gd
+++ vinculacionppp/escenas/Menudeopciones.gd
@@ -1,5 +1,20 @@
 extends Control
+onready var btncabeza=get_node("partesdecara")
+onready var btnhuesos=get_node("partesdehuesos")
+onready var btnorganos=get_node("partesorganos")
+onready var botn=get_node("CuerpoEntero")
+func _ready():
+	btncabeza.connect("pressed",self,"_llamarCabeza")
+	btnhuesos.connect("pressed",self,"_llamarHuesos")
+	btnorganos.connect("pressed",self,"_llamarOrganos")
+	botn.connect("pressed",self,"_on_CuerpoEntero_pressed")
 
+func _llamarCabeza():
+	get_tree().change_scene("res://escenas/niñacabeza.tscn")
+func _llamarHuesos():
+	get_tree().change_scene("res://escenas/niñaHuesos.tscn")
+func _llamarOrganos():
+	get_tree().change_scene("res://escenas/niñaOrganos.tscn")
 
 
 func _on_CuerpoEntero_pressed():
